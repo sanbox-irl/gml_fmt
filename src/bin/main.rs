b82b4e56df0de4022240a27db31b26b7cfb1332a
Jonathan Spira <jjspira@Jonathans-Mini.lan>
2019-08-01 15:17:41 -0400

    Will read current directory if no path is provided.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,17 +8,20 @@ fn main() {
         .version("0.1.0")
         .author("Jonathan Spira <jjspira@gmail.com>")
         .about("Code Formatter for GML")
-        .arg(Arg::with_name("file").short("f").long("file"))
+        .arg(Arg::with_name("file").short("f").long("file").help("Sets gml_fmt to format a file"))
         .arg(
             Arg::with_name("PATH")
-                .help("Sets the path to the file or directory to use. Defaults to directory.")
+                .help("Sets the path to the file or directory to use. Leave blank to use the current directory.")
                 .index(1)
-                .required(true),
         )
         .get_matches();
 
-    let input_arg = matches.value_of("PATH").unwrap();
-    let input_path = PathBuf::from(input_arg);
+    let input_path = if matches.is_present("PATH") {
+        PathBuf::from(matches.value_of("PATH").unwrap())
+    } else {
+        std::env::current_dir().unwrap()
+    };
+
     let do_file = matches.is_present("file");
 
     match gml_fmt::run_config(input_path, do_file) {
