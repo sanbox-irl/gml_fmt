6a64e4ac5272679a690b9de3a21f69079676725c
Jonathan Spira <jjspira@Jonathans-Mini.lan>
2019-08-01 15:40:31 -0400

    Test changes

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET
     cross build --target $TARGET --release
@@ -13,12 +12,8 @@ main() {
 
     cross test --target $TARGET
     cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
 }
 
-# we don't run the "test phase" when doing deploys
 if [ -z $TRAVIS_TAG ]; then
     main
 fi
